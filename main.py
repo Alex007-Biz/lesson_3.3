ccaa52113090e6609ac10c687fd3f034506e1561
Alex007-Biz <bizukin@yandex.ru>
2024-03-23 11:14:12 +0300

    new img

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("")
+icon = pygame.image.load("img/img_1.jpg")
+pygame.display.set_icon(icon)
 
 
 running = True
